51b4bdc6574beb0dbc77b1f85faa17761f056b81
Rouxinol <italobessa.ib@gmail.com>
2016-07-16 11:52:04 -0300

    *php

--- jumple/index.php
+++ jumple/index.php
@@ -11,14 +11,14 @@
   <link rel="publisher" href="https://plus.google.com/116727413074390018571"/>
   <link rel="canonical" href="http://jumple.com.br/">
 
-  <meta itemprop="name" content="Jumple"/>
-  <meta itemprop="description" content="Identidade Visual e Publicidade Online"/>
+  <meta itemprop="name" content="Jumple - Identidade Visual e Publicidade Online"/>
+  <meta itemprop="description" content="Melhore sua marca, seu produto ou serviços com a Jumple, utilizamos as melhores ferramentas de Publicidade Online e Design! Trataremos seu projeto com carinho :)"/>
   <meta itemprop="image" content="http://jumple.com.br/imagens/compartilhamento-facebook.jpg"/>
   <meta itemprop="url" content="http://jumple.com.br/"/> 
        
   <meta property="og:type" content="article" />
-  <meta property="og:title" content="Jumple" />
-  <meta property="og:description" content="Identidade Visual e Publicidade Online" />
+  <meta property="og:title" content="Jumple - Identidade Visual e Publicidade Online" />
+  <meta property="og:description" content="Melhore sua marca, seu produto ou serviços com a Jumple, utilizamos as melhores ferramentas de Publicidade Online e Design! Trataremos seu projeto com carinho :)" />
   <meta property="og:image" content="http://jumple.com.br/imagens/compartilhamento-facebook.jpg" />
   <meta property="og:url" content="http://jumple.com.br/" />
   <meta property="og:site_name" content="Jumple" />
